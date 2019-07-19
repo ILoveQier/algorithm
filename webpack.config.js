bb4caf673721aba10e0122b761dbea07c34d118c
王超 <597584896@qq.com>
2019-07-19 17:59:08 +0800

    al

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,21 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    al: './src/algri.js',
+    reg: './src/regexp.js'
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    })
+  ],
   devServer: {
     contentBase: './dist'
   },
